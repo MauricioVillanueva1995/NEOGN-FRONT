d826268044a6b18f1db525a8ab9da1566797096a
MauricioVillanueva <mauriciogonzalovillanueva@hotmail.com>
2024-03-04 02:37:39 -0300

    try

--- src/components/DashboardUser/OrderHistoryDesktop.jsx
+++ src/components/DashboardUser/OrderHistoryDesktop.jsx
@@ -5,7 +5,7 @@ import SidebarUser from "./SidebarUser";
 import { AnimatePresence } from "framer-motion";
 import CartDesktop from "../Cart/CartDesktop";
 import Footer from "../Footer";
-import BGDark from "../../utils/images/background/BGDark.webp"
+import BGDark from "../../../src/utils/images/Background/BGDark.webp"
 
 const OrderHistoryDesktop = ({ modalOpenCart, closeCart }) => {
   const orderHistoryDesktopRef = useRef();

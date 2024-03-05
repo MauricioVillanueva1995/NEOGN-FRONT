d225dbabfcacad9ad173a551bfde62bea98c182c
MauricioVillanueva <mauriciogonzalovillanueva@hotmail.com>
2024-03-05 20:25:07 -0300

    imgs

--- src/components/Search/SearchBannerPaths.js
+++ src/components/Search/SearchBannerPaths.js
@@ -1,12 +1,22 @@
+import Controller from "../../utils/Images/SearchBanner/Controller.webp";
+import Headset from "../../utils/Images/SearchBanner/Controllers.webp"
+import Earbud from "../../utils/Images/SearchBanner/Innear.webp"
+import Keyboard from "../../utils/Images/SearchBanner/Controllers.webp"
+import Microphone from "../../utils/Images/SearchBanner/Controllers.webp"
+import Monitor from "../../utils/Images/SearchBanner/Controllers.webp"
+import Mouse from "../../utils/Images/SearchBanner/Controllers.webp"
+import Mousepad from "../../utils/Images/SearchBanner/Controllers.webp"
+
+
 const SearchBannerPaths = {
-  Monitors: "https://i.postimg.cc/VvDvnFqN/2.png",
-  Headsets: "https://assets.razerzone.com/eeimages/support/products/1408/1408_nari_ultimate.png",
-  Keyboards: "https://i.postimg.cc/vZFVL09P/hyperx-alloy-fps-rgb-gaming-keyboard.png",
-  Mice: "https://i.postimg.cc/KzRSYr9b/g502x-plus-gallery-2-black.png",
-  Mousepads: "https://i.postimg.cc/bwfMjRxW/powerplay-gallery-1-nb.png",
-  Controllers: "https://i.postimg.cc/SsLJN4jC/h525.png",
-  Earbuds: "https://i.postimg.cc/054mrkN1/h732.png",
-  Microphones: "https://i.postimg.cc/4yJnY0hM/70225-21-eb63a1bdb2d76969f816432054072838-1024-1024.png",
+  Monitors: {Monitor},
+  Headsets: {Headset},
+  Keyboards: {Keyboard},
+  Mice: {Mouse},
+  Mousepads: {Mousepad},
+  Controllers: {Controller},
+  Earbuds: {Earbud},
+  Microphones: {Microphone},
 };
 
 export default SearchBannerPaths;

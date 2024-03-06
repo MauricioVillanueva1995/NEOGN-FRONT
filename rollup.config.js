96be42c1bb004a6dab45aa268d6ed6978a877681
MauricioVillanueva <mauriciogonzalovillanueva@hotmail.com>
2024-03-06 19:04:26 -0300

    try

--- rollup.config.js
+++ rollup.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from 'vite';
 import { createVuePlugin } from 'vite-plugin-vue2';
 import { imageminPlugin } from './plugins/imageminPlugin';
 import { VitePWA } from 'vite-plugin-pwa';
-import image from '@rollup/plugin-image';
+import rollupPluginImage from '@rollup/plugin-image';
 
 export default defineConfig({
   plugins: [
     createVuePlugin(),
     imageminPlugin(),
     VitePWA(),
-    image({
+    rollupPluginImage({
       include: ['**/*.png', '**/*.jpg', '**/*.gif', '**/*.webp'],
       exclude: 'node_modules/**',
       dom:true,
